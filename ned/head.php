0edfc80855d78f2dec35f0d51a3c6b154a46bb14
Ned H <user@Users-Air.fios-router.home>
2022-05-16 02:09:12 -0400

    May 16 update
    Made important changes to personal site, with a new SVG in revision 1 for large-screen viewing.

---
+++ ned/head.php
@@ -0,0 +1,15 @@
+<!doctype html>
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!--Metadata specific items -->
+
+	<title>Ned Haskins - Web Design</title>
+
+	<meta name='description' content='A website showcasing my web design work, my thought processes, some inspiring links, and other interesting items.'>
+
+	<meta property='og:image' content='https://peprojects.dev/alpha-4/ned/images/meta-image.jpg'> <!-- This link needs to be the 'absolute' or entire file path written out! -->
+
+</head>
---
+++ ned/index.php
@@ -0,0 +1,7 @@
+<!doctype html>
+
+<html lang="en">
+
+<?php include('head.php'); ?>
+
+<?php include('body.php'); ?>
