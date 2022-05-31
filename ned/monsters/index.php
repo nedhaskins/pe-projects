eecd656470e5e91a51d6264cff51a030d3a665eb
Ned H <user@Users-Air.fios-router.home>
2022-05-31 18:09:18 -0400

    Monster adoption project added and updated.

---
+++ ned/monsters/index.php
@@ -0,0 +1,53 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="stylesheet" href="style.css">
+	<meta property="og:image" content="https://peprojects.dev/alpha-4/ned/monsters/portraits/orangina.jpg">
+		<meta name="description" content="Matching monsters with humans since 2022.">
+	<title>Monster Adoption Agency Inc.</title>
+
+	<style>
+		background-color: black;
+	</style>
+</head>
+	<body>
+
+		<page-left>
+			<header>
+				<h1>MONSTERS</h1>
+				<h2>-for adoption-</h2>
+
+
+			</header>
+
+			<page-content>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+				<p>Contact one of our adoption specialists today!</p>
+
+				<button type="button">Contact</button>
+
+			</page-content>
+
+		</page-left>
+		<!--BEGINNING OF RIGHT SIDE / PHP PART OF CODE -->
+
+
+
+		<page-right>
+
+			<?php include('page-right.php'); ?>
+
+		</page-right>
+
+	</body>
+</html>
