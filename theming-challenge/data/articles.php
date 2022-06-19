c02b8ddffe0c48779f2e6c3b4b9f9513cd0e307a
Ned H <ed.hask85@gmail.com>
2022-06-19 17:29:21 -0400

    Updated theme project files

---
+++ theming-challenge/data/articles.php
@@ -0,0 +1,34 @@
+<?php
+
+$articles = [
+	[
+		"heading" => "Article or product",
+		"description" => "Here's a little info to help you understand if this is a thing you want to know about.",
+		"thumbnail" => "house-1.jpeg",
+	],
+	[
+		"heading" => "Destination or option",
+		"description" => "Here's a little info to help you understand if this is a thing you want to know about.",
+		"thumbnail" => "house-2.jpeg",
+	],
+	[
+		"heading" => "Story or something",
+		"description" => "Here's a little info to help you understand if this is a thing you want to know about.",
+		"thumbnail" => "house-3.jpeg",
+	],
+	[
+		"heading" => "Banana tandori",
+		"description" => "Here's a little info to help you understand if this is a thing you want to know about.",
+		"thumbnail" => "house-4.jpeg",
+	],
+	[
+		"heading" => "Bar B Q",
+		"description" => "Here's a little info to help you understand if this is a thing you want to know about.",
+		"thumbnail" => "house-5.jpeg",
+	],
+	[
+		"heading" => "Marsala",
+		"description" => "Here's a little info to help you understand if this is a thing you want to know about.",
+		"thumbnail" => "house-6.jpeg",
+	],
+];
---
+++ theming-challenge/modules/graphic-diptych/template.php
@@ -0,0 +1,14 @@
+
+<graphic-diptych>
+
+	<picture>
+		<img src='images/shoreline.jpeg' alt='$todo'>
+	</picture>
+	
+	<text-content>
+		<h1 class='loud-voice'>This is a graphic diptych</h1>
+
+		<p>What a fun thing that we have here</p>
+	</text-content>
+
+</graphic-diptych>
