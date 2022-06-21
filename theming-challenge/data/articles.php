35b8d39cc575ada5f3c74fded5001431844e0057
Ned H <ed.hask85@gmail.com>
2022-06-21 18:19:34 -0400

    Theme challenge
    Added cards, spacing, complete dark mode

--- theming-challenge/data/articles.php
+++ theming-challenge/data/articles.php
@@ -4,31 +4,31 @@ $articles = [
 	[
 		"heading" => "Article or product",
 		"description" => "Here's a little info to help you understand if this is a thing you want to know about.",
-		"thumbnail" => "house-1.jpeg",
+		"thumbnail" => "house-1.jpg",
 	],
 	[
 		"heading" => "Destination or option",
 		"description" => "Here's a little info to help you understand if this is a thing you want to know about.",
-		"thumbnail" => "house-2.jpeg",
+		"thumbnail" => "house-2.jpg",
 	],
 	[
 		"heading" => "Story or something",
 		"description" => "Here's a little info to help you understand if this is a thing you want to know about.",
-		"thumbnail" => "house-3.jpeg",
+		"thumbnail" => "house-3.jpg",
 	],
 	[
 		"heading" => "Banana tandori",
 		"description" => "Here's a little info to help you understand if this is a thing you want to know about.",
-		"thumbnail" => "house-4.jpeg",
+		"thumbnail" => "house-4.jpg",
 	],
 	[
 		"heading" => "Bar B Q",
 		"description" => "Here's a little info to help you understand if this is a thing you want to know about.",
-		"thumbnail" => "house-5.jpeg",
+		"thumbnail" => "house-5.jpg",
 	],
 	[
 		"heading" => "Marsala",
 		"description" => "Here's a little info to help you understand if this is a thing you want to know about.",
-		"thumbnail" => "house-6.jpeg",
+		"thumbnail" => "house-6.jpg",
 	],
 ];
