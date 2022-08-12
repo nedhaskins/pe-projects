e8c0bade119face9262218181980218e4dd87988
Ned H <ed.hask85@gmail.com>
2022-08-12 19:44:41 -0400

    exercise.php page created and hooked up

--- templates/modules/graphic-list/template.php
+++ templates/modules/graphic-list/template.php
@@ -3,7 +3,7 @@
 				<?php foreach ($section['items'] as $item) { ?>
 					<li class='list-item'>
 						<picture class='e4p-image'><?php include($item['image']); ?></picture>
-						<a href="?page=e4p&slug=<?=$item['slug']?>"><?=$item['name']?></a>
+						<a href="?page=<?=$item['page']?>&slug=<?=$item['slug']?>"><?=$item['name']?></a>
 					</li>
 				<?php } ?>
 			</ul>
--- templates/pages/exercise.php
+++ templates/pages/exercise.php
@@ -1,23 +1,27 @@
 <?php
-$json = file_get_contents('data/pages/projects.json');
+$json = file_get_contents('data/pages/e4p.json');
 $pageData = json_decode($json, true);
 $sections = $pageData['sections'];
+
 
 ?>
 <a href="">Back to Exercises for Programmers</a>
 
-
+<section class='exercise'>
 <?php
 //this will be a function
 
-if( isset($_GET['id']) ) {
+foreach($sections as $section) {
+
+
+if( isset($_GET['slug']) ) {
 
 	foreach ($section['items'] as $item) {
-		$itemID = $item['id'];
+		$itemSlug = $item['slug'];
 
-		echo $itemID;
+		// echo $itemSlug;
 
-		if($itemID == $_GET['id']) {
+		if($itemSlug == $_GET['slug']) {
 			include("templates/modules/e4p/" . $item['slug'] . ".php");
 		}
 	}
@@ -25,6 +29,11 @@ if( isset($_GET['id']) ) {
 	include("index.php");
 }
 
+}
+
 ?>
+
+
+</section>
 
 <!--Uses same logic as the about --> subpage.
