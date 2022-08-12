4069cfe2cf9b8ae566bbeefab381971b04baff78
Ned H <ed.hask85@gmail.com>
2022-08-11 22:46:03 -0400

    Create navbar.  Rope off before index layout change.

--- index.php
+++ index.php
@@ -14,33 +14,18 @@
 <inner-column>
 
 	<gridbox class='navbar'>
-
-	<nav>
-		<a class='link' href="?page=home" alt="">home</a>
-		<a class='link' href="?page=about" alt="">about me</a>
-		<a class='link' href="?page=projects" alt="">projects</a>
-		<a class='link' href="?page=the-lab" alt="">the lab</a>
-		<a class='link' href="http://nedhaskins.substack.com/" alt="">substack</a>
-		<a class='link' href="?page=style-guide" alt="">style guide</a>
-		<a class='link' href="?page=contact" alt="">contact</a>
-	</nav>
-
+		<?php include('navbar.php'); ?>
 	</gridbox>
 
-
 	<gridbox class='logo'>
-
 		<artifact>
 			<?php include('images/full-logo.svg'); ?>
 		</artifact>
-
 	</gridbox>
 
 
 	<gridbox class='page-content'>
-
 		<?php fetchPage(); ?>
-
 	</gridbox>
 
 </inner-column>
