34eb99071e8c35fb6c9216eb4bb02380c9e697b6
Ned H <ed.hask85@gmail.com>
2022-08-14 19:20:46 -0400

    All layout data is visible on pages

---
+++ templates/modules/layouts/layout-5/template.php
@@ -0,0 +1,44 @@
+
+<header class='site-header'>
+
+	<h1 class='heading-voice'>Tattoo Shop 1</h1>
+
+
+	<nav class='navbar'>
+		<ol>
+			<li><a href='#'>Home</a></li>
+			<li><a href='#'>Artists</a></li>
+			<li><a href='#'>Book an Artist</a></li>
+			<li><a href='#'>Aftercare</a></li>
+			<li><a href='#'>Reviews</a></li>
+		</ol>
+
+	</nav>
+
+</header>
+
+
+
+<photo-module>
+	
+
+
+
+</photo-module>
+
+
+
+
+<footer>
+	<footer-top>
+		<h2 class='heading-voice'>Tattoo Shop</h2>
+		<p class='copy-voice'>We make great tattoos...<br>and we're agreeable people to boot.</p>
+		<call-to-action>
+			<box></box>
+			<h4 class='copy-voice'>Call us today!</h4>
+		</call-to-action>
+	</footer-top>
+		
+	<button type='submit' name='submitted'>Book an Artist</button>
+
+</footer>
---
+++ the-lab/layout-garden/layouts-5-6/template.php
@@ -0,0 +1,24 @@
+
+	<link rel='stylesheet' href='../css/site.css'>
+
+	<section class='two-pillars'>
+
+	<div class='index-line'></div>
+
+		<inner-column>
+
+			<?php include('templates/modules/two-pillars/two-pillars.php'); ?>
+
+		</inner-column>
+	</section>
+
+	<section class='loose-screw'>
+
+	<div class='index-line'></div>
+
+		<inner-column>
+
+			<?php include('templates/modules/loose-screw/loose-screw.php'); ?>
+
+		</inner-column>
+	</section>
