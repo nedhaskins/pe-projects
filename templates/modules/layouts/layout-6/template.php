587f6002c760dcc5292a567f4589b7b5cc6cef7f
Ned H <ed.hask85@gmail.com>
2022-08-16 11:45:14 -0400

    Layouts 5 and 6 styling

--- templates/modules/layouts/layout-6/template.php
+++ templates/modules/layouts/layout-6/template.php
@@ -11,19 +11,19 @@
 
 	<picture-section>	
 		<picture class='shop-photo'>
-			<img src='../templates/modules/loose-screw/images/shirt-1.webp' alt="">
+			<img src='templates/modules/layouts/layout-6/images/shirt-1.webp' alt="">
 			<figcaption class='copy-voice'>Apparel</figcaption>
 		</picture>
 
 		<!--The filepath is from INDEX, not from where the PHP script lives!-->
 
 		<picture class='shop-photo'>
-			<img src='../templates/modules/loose-screw/images/shirt-2.webp' alt="">
+			<img src='templates/modules/layouts/layout-6/images/shirt-2.webp' alt="">
 			<figcaption class='copy-voice'>Manager's Collection</figcaption>
 		</picture>
 
 		<picture class='shop-photo'>
-			<img src='../templates/modules/loose-screw/images/poster-1.webp' alt="">
+			<img src='templates/modules/layouts/layout-6/images/poster-1.webp' alt="">
 			<figcaption class='copy-voice'>Posters</figcaption>
 		</picture>
 	</picture-section>
@@ -35,27 +35,27 @@
 	<h3 class='heading-voice'>Tattoos</h3>
 	<picture-section>	
 		<picture class='shop-photo'>
-			<img src='../templates/modules/loose-screw/images/mandalorian.webp' alt="">
+			<img src='templates/modules/layouts/layout-6/images/mandalorian.webp' alt="">
 		</picture>
 
 		<picture class='shop-photo'>
-			<img src='../templates/modules/loose-screw/images/bird.webp' alt="">
+			<img src='templates/modules/layouts/layout-6/images/bird.webp' alt="">
 		</picture>
 
 		<picture class='shop-photo'>
-			<img src='../templates/modules/loose-screw/images/octolantern.webp' alt="">
+			<img src='templates/modules/layouts/layout-6/images/octolantern.webp' alt="">
 		</picture>
 
 		<picture class='shop-photo'>
-			<img src='../templates/modules/loose-screw/images/tiger-fish.webp' alt="">
+			<img src='templates/modules/layouts/layout-6/images/tiger-fish.webp' alt="">
 		</picture>
 
 		<picture class='shop-photo'>
-			<img src='../templates/modules/loose-screw/images/mandalorian.webp' alt="">
+			<img src='templates/modules/layouts/layout-6/images/mandalorian.webp' alt="">
 		</picture>
 
 		<picture class='shop-photo'>
-			<img src='../templates/modules/loose-screw/images/bird.webp' alt="">
+			<img src='templates/modules/layouts/layout-6/images/bird.webp' alt="">
 		</picture>
 	</picture-section>
 
@@ -68,43 +68,43 @@
 	<h3 class='heading-voice'>Artists</h3>
 	<picture-section>	
 		<picture class='shop-photo'>
-			<img src='../templates/modules/loose-screw/images/mandalorian.webp' alt="">
+			<img src='../templates/modules/layouts/layout-6/images/mandalorian.webp' alt="">
 		</picture>
 
 		<picture class='shop-photo'>
-			<img src='../templates/modules/loose-screw/images/bird.webp' alt="">
+			<img src='../templates/modules/layouts/layout-6/images/bird.webp' alt="">
 		</picture>
 
 		<picture class='shop-photo'>
-			<img src='../templates/modules/loose-screw/images/octolantern.webp' alt="">
+			<img src='../templates/modules/layouts/layout-6/images/octolantern.webp' alt="">
 		</picture>
 
 		<picture class='shop-photo'>
-			<img src='../templates/modules/loose-screw/images/tiger-fish.webp' alt="">
+			<img src='../templates/modules/layouts/layout-6/images/tiger-fish.webp' alt="">
 		</picture>
 
 		<picture class='shop-photo'>
-			<img src='../templates/modules/loose-screw/images/mandalorian.webp' alt="">
+			<img src='../templates/modules/layouts/layout-6/images/mandalorian.webp' alt="">
 		</picture>
 
 		<picture class='shop-photo'>
-			<img src='../templates/modules/loose-screw/images/bird.webp' alt="">
+			<img src='../templates/modules/layouts/layout-6/images/bird.webp' alt="">
 		</picture>
 
 		<picture class='shop-photo'>
-			<img src='../templates/modules/loose-screw/images/octolantern.webp' alt="">
+			<img src='../templates/modules/layouts/layout-6/images/octolantern.webp' alt="">
 		</picture>
 
 		<picture class='shop-photo'>
-			<img src='../templates/modules/loose-screw/images/tiger-fish.webp' alt="">
+			<img src='../templates/modules/layouts/layout-6/images/tiger-fish.webp' alt="">
 		</picture>
 
 		<picture class='shop-photo'>
-			<img src='../templates/modules/loose-screw/images/mandalorian.webp' alt="">
+			<img src='../templates/modules/layouts/layout-6/images/mandalorian.webp' alt="">
 		</picture>
 
 		<picture class='shop-photo'>
-			<img src='../templates/modules/loose-screw/images/bird.webp' alt="">
+			<img src='../templates/modules/layouts/layout-6/images/bird.webp' alt="">
 		</picture>
 	</picture-section>
 
