e6d970d1871adbece17fd956d8d2f413ed2ebc94
Ned H <ed.hask85@gmail.com>
2022-08-26 11:55:49 -0400

    Styling #3 8.26

--- templates/modules/404/template.php
+++ templates/modules/404/template.php
@@ -1,6 +1,6 @@
 <section class='four-oh-four'>
 
-<text-wrapper class='four-oh-four'>
+<text-wrapper>
 	<h1 class='title-voice'><?=$section['heading']?></h1>
 	<h2 class='attention-voice'><?=$section['copy']?></h2>
 </text-wrapper>
