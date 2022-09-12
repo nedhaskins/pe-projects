c07e24606101e3979fc98ac94cc812c5d9121381
Ned H <ed.hask85@gmail.com>
2022-09-12 02:40:52 -0400

    Faqs 1 mostly done

--- figma-to-code/templates/modules/faqs/template.php
+++ figma-to-code/templates/modules/faqs/template.php
@@ -5,40 +5,44 @@
 		<?php include('components/header-and-text/template.php'); ?>
 
 
-		<div class='dropdowns'>
 
 
-				<article>
-					<details>
-						<summary>How soon will my order ship?</summary>Elementum ullamcorper felis nulla scelerisque. Nunc enim nunc mattis leo massa.
-					</details>
-				</article>
+			<div class='dropdowns'>
 
-				<article>
-					<details>
-						<summary>Are there any warranty exclusions?</summary>Elementum ullamcorper felis nulla scelerisque. Nunc enim nunc mattis leo massa.
-					</details>
-				</article>
 
-				<article>
-					<details>
-						<summary>Are there any return exclusions?</summary>Elementum ullamcorper felis nulla scelerisque. Nunc enim nunc mattis leo massa.
-					</details>
-				</article>
+					<article>
+						<details>
+							<summary>How soon will my order ship?</summary>Elementum ullamcorper felis nulla scelerisque. Nunc enim nunc mattis leo massa.
+						</details>
+					</article>
 
-				<article>
-					<details>
-						<summary>Will I be charged for a replacement item?</summary>Elementum ullamcorper felis nulla scelerisque. Nunc enim nunc mattis leo massa.
-					</details>
-				</article>
+					<article>
+						<details>
+							<summary>Are there any warranty exclusions?</summary>Elementum ullamcorper felis nulla scelerisque. Nunc enim nunc mattis leo massa.
+						</details>
+					</article>
 
-				<article>
-					<details>
-						<summary>How can I track my orders and payment?</summary>Elementum ullamcorper felis nulla scelerisque. Nunc enim nunc mattis leo massa.
-					</details>
-				</article>
+					<article>
+						<details>
+							<summary>Are there any return exclusions?</summary>Elementum ullamcorper felis nulla scelerisque. Nunc enim nunc mattis leo massa.
+						</details>
+					</article>
 
-		</div>
+					<article>
+						<details>
+							<summary>Will I be charged for a replacement item?</summary>Elementum ullamcorper felis nulla scelerisque. Nunc enim nunc mattis leo massa.
+						</details>
+					</article>
+
+					<article>
+						<details>
+							<summary>How can I track my orders and payment?</summary>Elementum ullamcorper felis nulla scelerisque. Nunc enim nunc mattis leo massa.
+						</details>
+					</article>
+
+			</div>
+
+
 
 	</inner-column>
 
