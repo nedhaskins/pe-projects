5c0b30c58586bbe6427bffaa4e57046953b0aba2
Ned <ed.hask85@gmail.com>
2022-11-04 11:58:42 -0400

    cello app name optimizing, fixing optional skill entry

--- projects/cello-exercise-app/sat-cello-class-app.js
+++ projects/cello-exercise-app/sat-cello-class-app.js
@@ -13,71 +13,53 @@ class CelloExerciseBook {
 		this.addEventListeners();
 	}
 
-	addExercise(id, skills) {
+	addExercise(number, skills) {
 		//initialize the variable to be added
 		const exercise = {
-			id: id,
-			skills: skills,
+			number: number,
+			skills: skills || [],
 			complete: false,
 		};
 
 		this.exercises = [...this.exercises, exercise];
 		//does the same thing as the push method
-		console.log(`Added exercise #${id} to ${this.title}`);
+		console.log(`Added exercise #${number} to ${this.title}`);
 		this.renderExercises(this.exercises);
 	}
 
 	//this function may not be wholly needed for this app - look at all the repetition...
-	findExerciseById(searchId) { //remember 'searchId' can be any word!
+	findExerciseByNumber(number) { //remember 'searchId' can be any word!
 		return this.exercises.find( function(exercise) {
-			return (exercise.id == searchId);
+			return (exercise.number == number);
 		})	
 	}
 
-	showExercise(searchId) {
-		const found = this.findExerciseById(searchId);
+	logExercise(number) {
+		const found = this.findExerciseByNumber(number);
 		if(found) {
-			console.log(`This is exercise #${found.id} from ${this.title}`);
+			console.log(`This is exercise #${found.number} from ${this.title}`);
 		} else {
 			alert(`This exercise isn't in the book yet...`);
 		}
 	}
 
-	removeExercise(id) {
+	removeExercise(number) {
 			const filtered = this.exercises.filter( function(exercise) {
-				return exercise.id != id;
+				return exercise.number != number;
 			})
 			this.exercises = [...filtered];
 			this.renderExercises(this.exercises);
-			console.log(`Exercise #${id} was taken out of the book.`);
+			console.log(`Exercise #${number} was taken out of the book.`);
 	}
 
-	completeExercise(id) {
+	completeExercise(number) {
 		for (let i = 0; i < this.exercises.length; i++) {
-			if ( this.exercises[i].id == id ) {
+			if ( this.exercises[i].number == number ) {
 				this.exercises[i].complete = true;
-				console.log(`Exercise #${this.exercises[i].id} is marked complete.`)
+				console.log(`Exercise #${this.exercises[i].number} is marked complete.`)
 			}
 		}
 		this.renderExercises(this.exercises);
-	}
-
-	renderExercise(exercise) {
-		var skills = exercise.skills;
-
-			return `
-				<li data-id="${exercise.id}">
-					<exercise-card class=${exercise.complete ? "complete" : ""}>
-						<h2>Exercise #${exercise.id}</h2>
-						<ul> ${ skills } </ul>
-						<actions>
-							<button>Add skill</button>
-							<button>Remove exercise</button>
-							<button rel="toggle">Mark complete</button>
-						</actions>
-					</exercise-card>
-				</li>
-			`;
 	}
 
 	renderExercises() {
@@ -89,33 +71,55 @@ class CelloExerciseBook {
 		this.$output.innerHTML = template;
 	}
 
-	renderSkills(skills) {
-		skills.forEach( function (skill) {
-			return `<li>${skill}</li>`;
-		});
-	}	
+	renderExercise(exercise) {
+		var skills = exercise.skills;
 
-	showSkills(searchId) {
+			return `
+				<li data-id="${exercise.number}">
+					<exercise-card class=${exercise.complete ? "complete" : ""}>
+						<h2>Exercise #${exercise.number}</h2>
+						<actions>
+							<button>Add skill</button>
+							<button>Remove exercise</button>
+							<button rel="toggle">Mark complete</button>
+						</actions>
+					</exercise-card>
+				</li>
+			`;
+	}
+
+	
+
+	// renderSkills(skills) {
+	// 	skills.forEach( function (skill) {
+	// 		return `<li>${skill}</li>`;
+	// 		console.log("test");
+	// 	});
+	// }
+
+
+
+	logSkills(number) {
 		const found = this.exercises.find( function(exercise) {
-			return (exercise.id == searchId);
+			return (exercise.number == number);
 		})
 		if(found) {
 			var skills = found.skills;
-			console.log(`Exercise #${searchId} from ${this.title} focuses on:`);
+			console.log(`Exercise #${number} from ${this.title} focuses on:`);
 			skills.forEach( function(skill) {
 				console.log(skill);
 			});
 		}	
 	}
 
-	addSkill(searchId, skill) {
+	addSkill(number, skill) {
 		const found = this.exercises.find( function(exercise) {
-			return (exercise.id == searchId);
+			return (exercise.number == number);
 		})
 		if(found) {
 			var skills = found.skills;
 			skills.push(skill);
-			console.log(`Added ${skill} to exercise #${searchId}.`);
+			console.log(`Added ${skill} to exercise #${number}.`);
 		}
 	}
 
@@ -152,7 +156,7 @@ const popperHighSchool = new CelloExerciseBook("David Popper", "High School of C
 
 
 
-popperHighSchool.addExercise(1, ["triplets", "spicatto"]);
+popperHighSchool.addExercise(1, ["triplets", "spiccato"]);
 popperHighSchool.addExercise(2, ["slurs", "large shifts"]);
 popperHighSchool.addExercise(3, ["intonation", "chromatic runs"]);
 
@@ -162,4 +166,7 @@ popperHighSchool.addExercise(3, ["intonation", "chromatic runs"]);
 
 // popperHighSchool.showSkills(2); //works
 
-// popperHighSchool.addSkill(2, "chromatic runs"); //works
+popperHighSchool.addExercise(10);
+popperHighSchool.addSkill(10, "key changes");
+popperHighSchool.addSkill(2, "chromatic runs"); //works
+popperHighSchool.addSkill(2, "chromatic runs"); //works
