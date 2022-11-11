9e464f84f705965167d20acdce4cfbba83e6c27e
Ned <ed.hask85@gmail.com>
2022-11-11 11:08:59 -0500

    Added metadata to crud

--- projects/highwaysofvirginia/index.php
+++ projects/highwaysofvirginia/index.php
@@ -8,6 +8,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="css/style.css">
+    <meta property='og:image' content='https://peprojects.dev/alpha-4/ned/projects/php-crud/images/i-95.jpeg'> 
+    <meta name='description' content='Resource for information on Virginia highways and byways.'>
     <title></title>
 </head>
 <body>
