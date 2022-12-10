b5a79153126df8446c84ff679e20fdb8fed48da3
Ned <ed.hask85@gmail.com>
2022-12-09 19:57:29 -0500

    Node server is working

--- projects/node-js-projects/new-server/index.js
+++ projects/node-js-projects/new-server/index.js
@@ -1,66 +1,117 @@
 import http from "node:http";
 import fs from "node:fs";
 
-var projectFiles = []
+var dirFiles = []
 
 fs.readdir('./', function(error, files) {
 	if(error) return console.log('error', error);
-	console.log(files);
-	
+	files.forEach( function(file) {
+		if ( file.includes('html') ) {
+			dirFiles.push(file);
+		}
+	})
+	console.log(dirFiles);
 });
 
+// const server = http.createServer( function(request, response) {
+		
+// 	response.setHeader('Content-Type', 'text/html');
 
+// 	dirFiles.forEach(function(file) {
 
-
-
-
-
+// 		if (request.url == file) {
+// 			fs.readFile(file, 'utf8', (err, data) => {
+// 				if(err) {
+// 					return console.error(err);
+// 				}
+// 				response.statusCode = '200';
+// 				response.write(data);
+// 				response.end();
+// 			});
+// 		}
+// 	 })
+// });
 
 const server = http.createServer( function(request, response) {
 		
 	response.setHeader('Content-Type', 'text/html');
 
-	if (request.url == '/home') {
-		fs.readFile('home.html', 'utf8', (err, data) => {
-			if(err) {
-				return console.error(err);
-			}
-			response.statusCode = '200';
-			response.write(data);
-			response.end();
-		});
-	} else if(request.url == '/contact') {
-		fs.readFile('contact.html', 'utf8', (err, data) => {
-			if(err) {
-				return console.error(err);
-			}
-			response.statusCode = '200';
-			response.write(data);
-			response.end();
-		});
+	dirFiles.forEach( function(file) {
 
-	} else if(request.url == '/about.html') {
-		fs.readFile('about.html', 'utf8', (err, data) => {
-			if(err) {
-				return console.error(err);
-			}
-			response.statusCode = '200';
-			response.write(data);
-			response.end();
-		});
+		
+		//note the difference between file name and path name
 
-	} else if(request.url == '/404') {
-		fs.readFile('404.html', 'utf8', (err, data) => {
-			if(err) {
-				return console.error(err);
-			}
-			response.statusCode = '200';
-			response.write(data);
-			response.end();
-		});
-	}
+		if (request.url == `/${file}`) {
+			fs.readFile(file, 'utf8', (err, data) => {
+				if(err) {
+					return console.error(err);
+				}
+				response.statusCode = '200';
+				response.write(data);
+				response.end();
+			});
+	 	}
+	})
+
+
+	response.statusCode = '404';
+	response.end();		
 });
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 	if (request.url == '/home') {
+// 		fs.readFile('home.html', 'utf8', (err, data) => {
+// 			if(err) {
+// 				return console.error(err);
+// 			}
+// 			response.statusCode = '200';
+// 			response.write(data);
+// 			response.end();
+// 		});
+// 	} else if(request.url == '/contact') {
+// 		fs.readFile('contact.html', 'utf8', (err, data) => {
+// 			if(err) {
+// 				return console.error(err);
+// 			}
+// 			response.statusCode = '200';
+// 			response.write(data);
+// 			response.end();
+// 		});
+
+// 	} else if(request.url == '/about.html') {
+// 		fs.readFile('about.html', 'utf8', (err, data) => {
+// 			if(err) {
+// 				return console.error(err);
+// 			}
+// 			response.statusCode = '200';
+// 			response.write(data);
+// 			response.end();
+// 		});
+
+// 	} else if(request.url == '/404') {
+// 		fs.readFile('404.html', 'utf8', (err, data) => {
+// 			if(err) {
+// 				return console.error(err);
+// 			}
+// 			response.statusCode = '200';
+// 			response.write(data);
+// 			response.end();
+// 		});
+// 	}
+// });
 
 const PORT = 7777;
 const HOSTNAME = 'localhost';
@@ -68,3 +119,6 @@ const HOSTNAME = 'localhost';
 server.listen(PORT, HOSTNAME, function() {
 	console.log(`Server is running at http://${HOSTNAME}:${PORT}`);
 });
+
+
+
