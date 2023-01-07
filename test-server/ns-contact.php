2a9e11be719edb3cf867c83a46db12a17aa3dcec
Ned <ed.hask85@gmail.com>
2023-01-07 11:56:41 -0500

    edd error function to form test

--- test-server/ns-contact.php
+++ test-server/ns-contact.php
@@ -1,5 +1,22 @@
 <?php 
 
+function showErrors() {
+	ini_set('display_errors', '1');
+	ini_set('display_startup_errors', '1');
+	error_reporting(E_ALL);
+}
+
+showErrors();
+
+function show($things) { //$things = any set variable
+	echo "<code class='show-code'>";
+	echo '<pre>';
+	print_r($things);
+	echo '</pre>';
+	echo '</code>';
+}
+
+
 $name = "";
 $emailAddress = "";
 $phoneNumber = "";
