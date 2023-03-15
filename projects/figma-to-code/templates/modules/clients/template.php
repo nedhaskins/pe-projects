5fcadc8139ac261c2761f653a1bc8e7fb8dbd482
Ned <ed.hask85@gmail.com>
2023-03-15 17:29:49 -0400

    Figma - more template 1 styling

--- projects/figma-to-code/templates/modules/clients/template.php
+++ projects/figma-to-code/templates/modules/clients/template.php
@@ -17,45 +17,33 @@
 			</div>
 		</div>
 
+		<div class="review-wrapper">
+			<div class="client-review">
+				<logo class="five-stars">
+					<?php include('images/five-stars.svg'); ?>
+				</logo>
+				<p class="review">“Orci vel eget in eu. Integer amet porttitor hendrerit etiam arcu, aliquet duis pretium consequat. Semper sed viverra enim ut nunc.”</p>
+				<div class="client-block">
+					<picture class="client-photo">
+						<img src='images/client-photo.jpeg' alt="Client photo">
+					</picture>
 
-		<div class="client-review">
-
-			<logo class="five-stars">
-				<?php include('images/five-stars.svg'); ?>
-			</logo>
-
-			<p class="review">“Orci vel eget in eu. Integer amet porttitor hendrerit etiam arcu, aliquet duis pretium consequat. Semper sed viverra enim ut nunc.”</p>
-
-
-			<div class="client-block">
-				<picture class="client-photo">
-					<img src='images/client-photo.jpeg' alt="Client photo">
-				</picture>
-
-				<p class='client-name'>This Guy Watson</p>
+					<p class='client-name'>This Guy Watson</p>
+				</div>
 			</div>
-
-		</div>
-
-		<div class="client-review">
-
-			<logo>
-				<?php include('images/five-stars.svg'); ?>
-			</logo>
-
-			<p class="review">“Orci vel eget in eu. Integer amet porttitor hendrerit etiam arcu, aliquet duis pretium consequat. Semper sed viverra enim ut nunc.”</p>
-
-			<div class="client-block">
-				<picture class="client-photo">
-					<img src='images/client-photo.jpeg' alt="Client photo">
-				</picture>
-
-				<p class='client-name'>This Guy Watson</p>
+			<div class="client-review">
+				<logo>
+					<?php include('images/five-stars.svg'); ?>
+				</logo>
+				<p class="review">“Orci vel eget in eu. Integer amet porttitor hendrerit etiam arcu, aliquet duis pretium consequat. Semper sed viverra enim ut nunc.”</p>
+				<div class="client-block">
+					<picture class="client-photo">
+						<img src='images/client-photo.jpeg' alt="Client photo">
+					</picture>
+					<p class='client-name'>This Guy Watson</p>
+				</div>
 			</div>
-
 		</div>
-
-
 
 		<div class="chevron-wrapper">
 			<div class="chevron-right">
