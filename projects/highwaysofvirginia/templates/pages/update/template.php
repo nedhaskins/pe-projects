b4f7ccd11ee947e25edbc2420c4ff3b0dd14e7d1
Ned <ed.hask85@gmail.com>
2023-03-15 10:52:46 -0400

    HOV update form styling!

--- projects/highwaysofvirginia/templates/pages/update/template.php
+++ projects/highwaysofvirginia/templates/pages/update/template.php
@@ -1,65 +1,55 @@
-<!--Current issues:  WHen the page refreshes, an entirely new image is copied and uploaded.
-The name of the image is not saved in the update form.
+<?php
+$currentHighwayId = $_GET['slug'];
+$highway = getHighwayById($currentHighwayId);
+$highwayShield = $highway['image'] ?? false;
 
--->
+if ( isset($_POST['submitted']) ) {
 
+	if($_POST['route-type'] == 'interstate') {
+		$name = 'Interstate';
 
-<div class="update-page">
+	} elseif($_POST['route-type'] == 'us-route') {
+		$name = 'US Route';
 
-	<h2>Update Route Info</h2>
-
-	<?php
-
-	// show($_FILES);
-
-	//Pulling in the current highway data (before updating)
-	$currentHighwayId = $_GET['slug'];
-	$highway = getHighwayById($currentHighwayId);
-	$highwayShield = $highway['image'] ?? false;
-
-	if ( isset($_POST['submitted']) ) {
-
-		if($_POST['route-type'] == 'interstate') {
-			$name = 'Interstate';
-
-		} elseif($_POST['route-type'] == 'us-route') {
-			$name = 'US Route';
-
-		} elseif($_POST['route-type'] == 'state-route') {
-			$name = 'State Route';
-		}
-
-		$highway = array(
-			'type' => $_POST['route-type'],
-			'number' => $_POST['route-number'],
-			'name' => $name . " " . $_POST['route-number'],
-			'length' => $_POST['length-in-miles'],
-			'startLocation' => $_POST['start-location'],
-			'endLocation' => $_POST['end-location'],
-			'description' => $_POST['description'],
-			'image' => uploadImageFile() ?? $highwayShield,
-		);
-
-		$highways = getHighways();
-		$highways[$currentHighwayId] = $highway;
-
-		$json = json_encode($highways);
-		file_put_contents('data/highways.json', $json);
-
-		echo "<p class='success'>The route was successfully updated!</p>";
-
+	} elseif($_POST['route-type'] == 'state-route') {
+		$name = 'State Route';
 	}
 
-	include('templates/components/form/template.php');
+	$highway = array(
+		'type' => $_POST['route-type'],
+		'number' => $_POST['route-number'],
+		'name' => $name . " " . $_POST['route-number'],
+		'length' => $_POST['length-in-miles'],
+		'startLocation' => $_POST['start-location'],
+		'endLocation' => $_POST['end-location'],
+		'description' => $_POST['description'],
+		'image' => uploadImageFile() ?? $highwayShield,
+	);
 
-	?>
+	$highways = getHighways();
+	$highways[$currentHighwayId] = $highway;
 
+	$json = json_encode($highways);
+	file_put_contents('data/highways.json', $json);
 
-	<picture class="update-photo">
-		<img src="<?=$highway['image']?>" alt="todo">
-	</picture>
+	echo "<p class='success'>The route was successfully updated!</p>";
 
+} ?>
 
-	<?php show($highway); ?>
-
-</div>
+<section class="update-page">
+	<page-left>
+		<text-wrapper>
+			<h2>Update Route Info</h2>
+		</text-wrapper>
+		<div class="photo-and-array">
+			<picture class="update-photo">
+				<img src="<?=$highway['image']?>" alt="todo">
+			</picture>
+			<?php show($highway); ?>
+		</div>
+		</div>
+	</page-left>
+	<page-right>
+		<?php include('templates/components/form/template.php'); ?>
+	</page-right>
+</section>
