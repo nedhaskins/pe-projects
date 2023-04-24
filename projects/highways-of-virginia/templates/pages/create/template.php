812b443688a122c52577d08287d9a10cddede8e9
Ned <ed.hask85@gmail.com>
2023-04-24 15:10:22 -0400

    Updated form and create-route page on HOV

--- projects/highways-of-virginia/templates/pages/create/template.php
+++ projects/highways-of-virginia/templates/pages/create/template.php
@@ -16,47 +16,55 @@ $response = "";
 ?>
 
 <section class="create-route">
+    <page-left>
+        <text-wrapper>
+            <h2 class="page-title">Create New Route</h2>
+        </text-wrapper>
 
-    <h2>Create New Route</h2>
+        <?php
 
-<?php
+            include('templates/components/form/template.php');
 
-    include('templates/components/form/template.php');
+            if ( isset($_POST['submitted']) ) {
 
-    if ( isset($_POST['submitted']) ) {
+                if($_POST['route-type'] == 'interstate') {
+                    $name = 'Interstate';
+                } elseif($_POST['route-type'] == 'us-route') {
+                    $name = 'US Route';
+                } elseif($_POST['route-type'] == 'state-route') {
+                    $name = 'State Route';
+                }
 
-        if($_POST['route-type'] == 'interstate') {
-            $name = 'Interstate';
-        } elseif($_POST['route-type'] == 'us-route') {
-            $name = 'US Route';
-        } elseif($_POST['route-type'] == 'state-route') {
-            $name = 'State Route';
-        }
+                $highway = array(
+                    'type' => $_POST['route-type'],
+                    'number' => $_POST['route-number'],
+                    'name' => $name . " " . $_POST['route-number'],
+                    'length' => $_POST['length-in-miles'],
+                    'startLocation' => $_POST['start-location'],
+                    'endLocation' => $_POST['end-location'],
+                    'description' => $_POST['description'],
+                    'image' => uploadImageFile()
+                );
 
-        $highway = array(
-            'type' => $_POST['route-type'],
-            'number' => $_POST['route-number'],
-            'name' => $name . " " . $_POST['route-number'],
-            'length' => $_POST['length-in-miles'],
-            'startLocation' => $_POST['start-location'],
-            'endLocation' => $_POST['end-location'],
-            'description' => $_POST['description'],
-            'image' => uploadImageFile()
-        );
+                $highways[uniqid('route')] = $highway;
 
-        $highways[uniqid('route')] = $highway;
+                $encoded = json_encode($highways);
 
-        $encoded = json_encode($highways);
+                if( file_put_contents('data/highways.json', $encoded) ) {
+                   // include('templates/pages/success/template.php');
+                  echo "<p class='success'>" . $highway['name'] . " was uploaded to the database.";
+                   // header("Location: templates/pages/success.php");
+                }
+            }
 
-        if( file_put_contents('data/highways.json', $encoded) ) {
-           // include('templates/pages/success/template.php');
-          echo "<p class='success'>" . $highway['name'] . " was uploaded to the database.";
-           // header("Location: templates/pages/success.php");
-        }
-    }
+        ?>
 
-?>
+    <output><?=$response?></output>
 
-<output><?=$response?></output>
-
+</page-left>
+<page-right>
+            <picture class='stock-photo'><img src="images/minneapolis-1.jpeg"></picture>
+        <picture class='stock-photo'><img src="images/us-11-photo.jpg"></picture>
+        <picture class='stock-photo'><img src="images/va-20-photo.jpeg"></picture>
+</page-right>
 </section>
