ebd0103d4b270df33135070b870da198f8ed18f1
Ned <ed.hask85@gmail.com>
2023-07-20 15:36:20 -0400

    Added some functions for the body and hid showErrors()

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@ function showErrors() {
   ini_set('display_startup_errors', '1');
   error_reporting(E_ALL);
 }
-showErrors();
+// showErrors();
 
 function queryString() {
 	return $_SERVER['QUERY_STRING'];
@@ -51,6 +51,16 @@ function fetchPage() {
   }
 }
 
+//Home page functions.
+
+function fetchStylesheet($page){
+  if($page == 'layout') {
+    echo "<link rel='stylesheet' href='styles/layout-garden.css'>";
+  } else {
+    echo "<link rel='stylesheet' href='styles/style.css'>";
+  }
+}
+
 function returnLayoutPage($sections) {
   foreach($sections as $section) {
     if($section['slug'] == $_GET['slug']) { ?>
@@ -60,3 +70,54 @@ function returnLayoutPage($sections) {
     }
   }
 }
+
+function fetchBodyContent($page) {
+  if($page == 'home' || $page == '404') { ?>
+    <body class='landing-404'>
+      <header>
+        <div class='site-menu'>
+          <nav id='navbar' class='navbar'>
+            <?php include('templates/components/navbar/main-navbar.php'); ?>
+          </nav>
+          <div class='logo'>
+            <?php include('images/full-logo.svg'); ?>
+          </div>
+        </div>
+      </header>
+      <main class='page-content'>
+        <?php fetchPage(); ?>
+      </main>
+    </body>
+  <?php } elseif($page == 'layout' && isset($_GET['slug'] ) ) { ?>
+    <body class='layout'>
+      <main class='page-content'>
+        <?php fetchPage(); ?>
+      </main>
+    </body>
+  <?php } elseif($page == 'exercise' && isset($GET['slug'] ) ) { ?>
+    <body class='e4p-detail'>
+      <main class='page-content'>
+        <?php fetchPage(); ?>
+      </main>
+    </body>
+  <?php } else { ?>            
+    <body class='index'>
+      <header>
+        <div class='site-menu'>
+          <nav class='navbar'>
+            <?php include('templates/components/navbar/main-navbar.php'); ?>
+          </nav>
+          <div class='logo'>
+            <?php include('images/full-logo.svg'); ?>
+          </div>
+        </div>
+      </header>
+
+      <main class='page-content'>
+          <?php fetchPage(); ?>
+      </main>
+    </body>
+  <?php }
+}
+
+// ^^^come back to this and finish it -- add vanilla JS and Vue options
--- index.php
+++ index.php
@@ -17,76 +17,10 @@ if( isset($_GET['slug']) ) {
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-
-	<?php if($page == 'layout') { ?>
-
-		<link rel='stylesheet' href='styles/layout-garden.css'>
-
-	<?php } else { ?>
-
-		<link rel='stylesheet' href='styles/style.css'>
-
-	<?php } ?>
-
+	<?php	fetchStylesheet($page); ?>
 	<title>Ned Haskins - Web Designer - Richmond, VA</title>
-
 </head>
 
-<?php if($page == 'home' || $page == '404') { ?>
-
-	<body class='landing-404'>
-		<header>
-			<div class='site-menu'>
-				<nav id='navbar' class='navbar'>
-					<?php include('templates/components/navbar/main-navbar.php'); ?>
-				</nav>
-				<div class='logo'>
-					<?php include('images/full-logo.svg'); ?>
-				</div>
-			</div>
-		</header>
-		<main class='page-content'>
-			<?php fetchPage(); ?>
-		</main>
-	</body>
-
-<?php } elseif($page == 'layout' && isset($_GET['slug'] ) ) { ?>
-
-	<body class='layout'>
-		<main class='page-content'>
-			<?php fetchPage(); ?>
-		</main>
-	</body>
-
-<?php } elseif($page == 'exercise' && isset($GET['slug'] ) ) { ?>
-
-	<body class='e4p-detail'>
-		<main class='page-content'>
-			<?php fetchPage(); ?>
-		</main>
-	</body>
-
-<!--come back to this and finish it -- add vanilla JS and Vue options-->
-
-<?php } else { ?>            
-
-	<body class='index'>
-		<header>
-			<div class='site-menu'>
-				<nav class='navbar'>
-					<?php include('templates/components/navbar/main-navbar.php'); ?>
-				</nav>
-				<div class='logo'>
-					<?php include('images/full-logo.svg'); ?>
-				</div>
-			</div>
-		</header>
-
-		<main class='page-content'>
-				<?php fetchPage(); ?>
-		</main>
-	</body>
-
-<?php } ?>
+<?php fetchBodyContent($page); ?>
 
 </html>
